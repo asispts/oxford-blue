34dac3cd5448e7857acec8dfc1a3e55d43204180
Asis Pattisahusiwa <pattisahusiwa@users.noreply.github.com>
2021-10-23 23:22:31 +0900

    Add javascript demo file

---
+++ demo/javascript.js
@@ -0,0 +1,40 @@
+const x = (x, y) => x * y;
+const x = (x, y) => { return x * y };
+
+// Create Objects
+const apples = {name: 'Apples'};
+// Create a new Map
+const fruits = new Map();
+// Add new Elements to the Map
+fruits.set(apples, 500);
+
+class Car {
+    constructor(brand) {
+        this._carname = brand;
+    }
+    get carname() {
+        return this._carname;
+    }
+    set carname(x) {
+        this._carname = x;
+    }
+    present() {
+        return 'I have a ' + this.carname;
+    }
+    static hello() {
+        return "Hello!!";
+    }
+}
+
+class Model extends Car {
+    constructor(brand, mod) {
+        super(brand);
+        this.model = mod;
+    }
+    show() {
+        return this.present() + ', it is a ' + this.model;
+    }
+}
+
+let myCar = new Model("Ford", "Mustang");
+document.getElementById("demo").innerHTML = myCar.show();

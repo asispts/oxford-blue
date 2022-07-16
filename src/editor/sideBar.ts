70f0a235f2fb053f134fad927744d85c099aefd7
Asis Pattisahusiwa <79239132+asispts@users.noreply.github.com>
2022-07-16 20:33:09 +0900

    Remove commented-out code

--- src/editor/sideBar.ts
+++ src/editor/sideBar.ts
@@ -2,7 +2,6 @@ import { editorColorDef } from "../color";
 
 const sidebar = {
   "sideBar.background": editorColorDef.bgColor,
-  // "sideBar.foreground": editorColorDef.fgColor,
   "sideBarTitle.foreground": editorColorDef.fgDark,
   "sideBarSectionHeader.background": editorColorDef.bgDark,
   "sideBarSectionHeader.foreground": editorColorDef.fgDark,

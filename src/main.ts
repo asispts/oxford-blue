65aa9b105012b2b2ac9305dcbdf0b00e9af64452
Asis Pattisahusiwa <79239132+asispts@users.noreply.github.com>
2023-01-23 15:59:28 +0900

    Remove semantic colors definition

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,22 @@
 import fs from "node:fs/promises";
 import editorColorGenerator from "./editorColorGenerator";
-import semanticGenerator from "./semanticGenerator";
 import tokenColorGenerator from "./tokenColorGenerator";
 
 (async function () {
-  const theme = {
-    name: "oxford-blue",
-    type: "dark",
-    semanticHighlighting: true,
-    colors: editorColorGenerator(),
-    tokenColors: tokenColorGenerator(),
-    semanticTokenColors: semanticGenerator(),
-  };
+    const theme = {
+        name: "oxford-blue",
+        type: "dark",
+        semanticHighlighting: true,
+        colors: editorColorGenerator(),
+        tokenColors: tokenColorGenerator(),
+    };
 
-  try {
-    await fs.writeFile(
-      "./oxford-blue-color-theme.json",
-      JSON.stringify(theme, null, 4)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await fs.writeFile(
+            "./oxford-blue-color-theme.json",
+            JSON.stringify(theme, null, 4)
+        );
+    } catch (error) {
+        console.log(error);
+    }
 })();
--- src/semanticGenerator.ts
+++
@@ -1,7 +0,0 @@
-import getSemanticToken from "./semantic/genericSemantic";
-
-export default function semanticGenerator(): Object {
-  const semantic = getSemanticToken();
-
-  return semantic.getData();
-}

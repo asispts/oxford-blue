b596ce8d55125c6c52400e2176e764e42e7cb724
Asis Pattisahusiwa <79239132+asispts@users.noreply.github.com>
2023-05-24 20:18:54 +0900

    Update statusbar color for debugging state

--- src/editor/statusBar.ts
+++ src/editor/statusBar.ts
@@ -4,4 +4,6 @@ export default function setStatusBar(data: EditorColorMap) {
   data.set("statusBar.background", editorColorDef.accent);
   data.set("statusBar.foreground", editorColorDef.accentFg);
   data.set("statusBarItem.hoverBackground", editorColorDef.accentHover);
+  data.set("statusBar.debuggingBackground", editorColorDef.secondary);
+  data.set("statusBar.debuggingForeground", editorColorDef.secondaryFg);
 }

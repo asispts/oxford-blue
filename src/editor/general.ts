c46fc4b70e44b312d0209c8d78dc54dc00727224
Asis Pattisahusiwa <79239132+asispts@users.noreply.github.com>
2023-05-26 10:10:24 +0900

    Refactor editor color and sort the definitions

---
+++ src/editor/general.ts
@@ -0,0 +1,5 @@
+import { editorColorDef } from '../color';
+
+export default function editorGeneralSetting(data: EditorColorMap) {
+  data.set('foreground', editorColorDef.fgColor);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,30 @@
-import fs from 'node:fs/promises';
-import editorColorGenerator from './editorColorGenerator';
+import { writeFile } from 'fs/promises';
 import tokenColorGenerator from './tokenColorGenerator';
+import * as editorModules from './editor';
 
-(async function () {
+const editorColors = (): Record<string, string> => {
+  const data: EditorColorMap = new Map();
+
+  Object.values(editorModules).forEach((module) => {
+    if (typeof module === 'function') {
+      module(data);
+    }
+  });
+
+  return Object.fromEntries(new Map([...data].sort()));
+};
+
+(async () => {
   const theme = {
     name: 'oxford-blue',
     type: 'dark',
     semanticHighlighting: true,
-    colors: editorColorGenerator(),
+    colors: editorColors(),
     tokenColors: tokenColorGenerator(),
   };
 
   try {
-    await fs.writeFile('./oxford-blue-color-theme.json', JSON.stringify(theme, null, 4));
+    await writeFile('./oxford-blue-color-theme.json', JSON.stringify(theme, null, 4));
   } catch (error) {
     console.log(error);
   }
